cd1cb030aa65dfcf8e9a0509c98199ca6099139e
Fabiano <fabiano.ssantos79@gmail.com>
2024-08-17 05:59:22 -0300

    feat: insert page by ID

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,8 +4,10 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -31,4 +33,8 @@ public class SerieController {
         return serieService.obterSeriesLancamentos();
     }
 
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id){
+        return serieService.obterSeriePorId(id);
+    }
 }
