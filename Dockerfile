99694afa1df2ef6d1297e0788078ef9ce5aafb08
Fabiano <fabiano.ssantos79@gmail.com>
2024-09-08 02:43:48 -0300

    feat: create enviroments

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ LABEL authors="Fabiano SSantos"
 
 RUN mkdir /app
 WORKDIR /app
-COPY target/*.jar /app/app.jar
+COPY target/screenmatch-0.0.1-SNAPSHOT.jar /app/app.jar
 EXPOSE 8080
 CMD ["java", "-jar", "/app/app.jar"]
--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchApplication {
-
+	//jdbc:postgresql://${DB_HOST_SCREENMATCH}/${DB_DATABASE_SCREENMATCH}
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 
